aab6085bda0138d3d17f7c5fb5eeb8aaec695052
Tahir Civan <69795597+nomadtahir@users.noreply.github.com>
2021-12-08 14:54:06 +0100

    Update SimplifyModifier.js

--- examples/SimplifyModifier.js
+++ examples/SimplifyModifier.js
@@ -2,7 +2,7 @@ import {
 	BufferGeometry,
 	Float32BufferAttribute,
 	Vector3
-} from 'three';
+} from './three.module.j';
 import * as BufferGeometryUtils from '../utils/BufferGeometryUtils.js';
 
 /**
